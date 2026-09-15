a826119da0533844c14814d5c318196f60859e3b
Maya Salama <m23salama@gmail.com>
2026-09-15 00:04:36 -0700

    add linear regression model

--- src/explore_data.py
+++ src/explore_data.py
@@ -98,3 +98,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("Training set:", X_train.shape)
 print("Testing set:", X_test.shape)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
